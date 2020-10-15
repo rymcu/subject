452b55136aaa30157ad56c968d606e1f03dd60e7
caterpillar-_-42 <gxt2048@163.com>
2020-10-15 17:03:49 +0800

    :bento: add thymeleaf

--- src/main/java/com/rymcu/subject/controller/QuestionController.java
+++ src/main/java/com/rymcu/subject/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.rymcu.subject.domain.SubjectQuestion;
 import com.rymcu.subject.service.SubjectQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,8 @@ public class QuestionController {
     }
 
     @GetMapping("/system/menu/menu")
-    public String getMenu(){
+    public String getMenu(Model model){
+        model.addAttribute("qwer","qwesad");
         return  "/system/menu/menu";
     }
 }
